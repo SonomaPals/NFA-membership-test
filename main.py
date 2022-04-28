31ca982dee8cf7efa4c06ad3c7db0592a7d56869
Katie Pell <pellk@.sonoma.edu>
2022-04-27 21:00:44 -0700

    currently just returns original NFA :(

--- main.py
+++ main.py
@@ -6,23 +6,32 @@ from re import I
 import outside.inside.knfa as knfa # Import Katie's knfa.py file --> uses convert_nfa function
 import outside.inside.parse as parse # Import Jorge's parse.py file --> uses parse function
 
+def empty(num_states, num_symbols):
+    empty_table = []
+    for i in range(num_states):
+        empty_table.append([])
+        for j in range(num_symbols):
+            empty_table[i].append([])
+
+    return empty_table
+
+
 def main():
     # regex = input("Enter a regular expression: ")
     regex =  "(a+a.b)*.(a+E)"
     print(parse.parse(regex))
 
-    # Current test code --> Uses example NFA
-    num_states = 3
-    num_symbols = 3 # Already includes epsilon
-    epsilon_NFA = knfa.NFA(num_states, num_symbols) # Constructor for empty transition table - filled in during conversion
+    # Example from: https://www.geeksforgeeks.org/conversion-of-epsilon-nfa-to-nfa/
+    transition_table = [[[4], [1], [0]], [[], [2], [1,3]], [[], [3], [2]], [[], [], [3]], [[5], [], [1, 2, 4]], [[3], [], [5]]]
 
-    # # Goal code --> template for when Garret outputs the transition table for the E-NFA
-    # num_states = len(gtable) # Number of nested lists in gtable (number of states)
-    # num_symbols = len(gtable[0]) # Number of nested lists in each nested list in gtable (number of symbols) --> ALREADY WILL INCLUDE E IN NUMBER OF COLUMNS
-    # epsilon_NFA = knfa.NFA(len(gtable), len(gtable[0]) - 1, gtable) # Use Garret's output as transition table input, removing E column
-   
-    epsilon_free_NFA = knfa.convert_nfa(epsilon_NFA)
-    epsilon_free_NFA.print()
-    
+
+    num_states = len(transition_table)
+    num_symbols = len(transition_table[0]) - 1 # Don't count the epsilon column
+
+    empty_table = empty(num_states, num_symbols)
+            
+    newNFA = knfa.NFA(num_states, num_symbols + 1, transition_table).remove_epsilons() # One extra symbol for epsilon
+    newNFA.print()
+
     
 main()
--- outside/inside/knfa.py
+++ outside/inside/knfa.py
@@ -5,78 +5,39 @@ def print_lists(all_lists):
         print(item)
 
 class NFA():
-    def __init__(self, num_states, num_symbols):
-        self.num_states = num_states
-        self.num_symbols = num_symbols
-
-        self.transition_table = []
-        for i in range(num_states):
-            self.transition_table.append([]) # Add that row to the table
-        for i in range(num_states):
-            for j in range(num_symbols):
-                self.transition_table[i].append([])
-        self.filled = False
-
     def __init__(self, num_states, num_symbols, transition_table):
         self.num_states = num_states
         self.num_symbols = num_symbols
-
         self.transition_table = transition_table
-        self.filled = True
 
     def print(self):
         print_lists(self.transition_table)
 
+    def remove_epsilons(self):
+        epsilon_closures = [] # Create a new list of epsilon closures
+        for state in self.transition_table:
+            epsilon_closures.append(state[-1]) # The last input symbol in each state row is the epsilon closure
+
+        empty_table = []
+        for i in range(self.num_states):
+            empty_table.append([])
+            for j in range(self.num_symbols):
+                empty_table[i].append([])
+
+
+        newNFA = NFA(self.num_states, self.num_symbols - 1, empty_table) # Create a new NFA with empty transition table
+        for k in range(self.num_states): # For every state in the NFA
+            for i in epsilon_closures[k]: # For every state in the epsilon closure of the current state
+                for j in range(self.num_symbols): # For each of the symbols allowed
+                    for item in self.transition_table[i][j]: # For each item in the epsilon closure of this state on this input symbol
+                        if item not in newNFA.transition_table[i][j]:
+                            newNFA.transition_table[i][j].append(item) # Get only unique items, add to list
+        return newNFA
+
+
 def get_closure(state, col, nfa):
     return nfa.transition_table[state][col]
 
-def populate_transitions(myNFA):
- # Transitions from state 0
-    myNFA.transition_table[0][0] = [0]           # Input 0
-    myNFA.transition_table[0][1] = []            # Input 1
-    myNFA.transition_table[0][2] = [0, 1, 2]     # Input 2 (epsilon)
-
-    # Transitions from state 1
-    # Inputs: 0 through 2
-    myNFA.transition_table[1][0] = []          # Input 0
-    myNFA.transition_table[1][1] = [1]         # Input 1
-    myNFA.transition_table[1][2] = [1, 2]     # Input 2 (epsilon)
-
-    # # Transitions from state 2
-    # # Inputs: 0 through 2
-    myNFA.transition_table[2][0] = [0]       # Input 0
-    myNFA.transition_table[2][1] = [1]      # Input 1
-    myNFA.transition_table[2][2] = [2]          # Input 2 (epsilon)
-
-    myNFA.filled = True
-
-def create_efree_NFA(myNFA):   
-    epsilon_closures = [] # Create a new list of epsilon closures
-    for state in myNFA.transition_table:
-        epsilon_closures.append(state[-1]) # The last input symbol in each state row is the epsilon closure
-
-    newNFA = NFA(myNFA.num_states, myNFA.num_symbols) # Create a new NFA with empty transition table
-
-
-    for k in range(myNFA.num_states):
-        for i in epsilon_closures[k]: # For every state in the epsilon closure of state 0
-            for j in range(myNFA.num_symbols): # For each of the symbols allowed
-                for item in myNFA.transition_table[i][j]: # For each item in the closure of this state on this input symbol
-                    if item not in newNFA.transition_table[i][j]:
-                        newNFA.transition_table[i][j].append(item) # Get only unique items, add to list
-
-
-    newNFA.filled = True # Mark that the NFA has its transition table filled in
-
-    return newNFA
-
-def convert_nfa(eNFA):
-    if not eNFA.filled:
-        populate_transitions(eNFA) # Add all the necessary transitions to the NFA based on the input
-    
-    eFreeNFA = create_efree_NFA(eNFA)
-
-    return eFreeNFA
 
     # # input_string = input("Enter a string to test: ")
     # input_string = "ababa"
