b800de2ba5c6e8a5a1b0374889862c8b630ac54e
Garret Mook <mookg@sonoma.edu>
2022-04-30 22:27:28 -0700

    saving work

--- genfa.py
+++ genfa.py
@@ -1,6 +1,9 @@
 # Main file for CS 454 Final Project
 # Garret Mook, Katie Pell, Jorge Calderon
 # Spring 2022
+import random 
+import numpy as np
+from tabulate import tabulate
 class genfa:
     def __init__(self):
         self.newlist = []
@@ -20,10 +23,21 @@ class genfa:
             if (countNumOfParen > 1):
                 if (self.newlist[x] == '('):
                     finalOpen = x + 1 # + 1 grabs the next index to get the char after the first opening (
-        print(self.newlist[finalOpen:firstClosed])           
+        inner = []
+        inner = self.newlist[finalOpen:firstClosed]
+        NumSymbols = 0
+        symbols = []
+        for x in range(len(inner)):
+            if inner[x] != '+':
+                NumSymbols += 1
+                symbols.append(inner[x])
+        numStates = NumSymbols * 2 #2 States 
+        w, h = NumSymbols, numStates
+        # a_2d_matrix = np.zeros((h,w))
+        # print(a_2d_matrix)
 
 def main():
-    s = "((a+b)c)*"
+    s = "((a+b).c)*" 
     newobject = genfa()
     newobject.Parse_Input(s)
     newobject.checkForMid()
