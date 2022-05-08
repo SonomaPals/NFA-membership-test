643e01b1ae32a5175b6600d26eda12e24105d4b3
Garret Mook <mookg@sonoma.edu>
2022-05-07 22:20:53 -0700

    added comments and function

--- genfa.py
+++ genfa.py
@@ -1,10 +1,29 @@
 # epsilonNFA file for CS 454 Final Project
 # Garret Mook, Katie Pell, Jorge Calderon
 # Spring 2022
+
+# Union = "+"
+# +1 to numStates
+# epsilon to start states of symbols between the "+" sign
+
+# Concatenation = "."
+# left side of "." symbol grab the final state
+# right side of "." symbol grab the initain state
+# set a epsilon transition from the final state of the left to the initial state of the right
+# Note: There can be more than 1 symbols on either side, i.e. (a+b).c
+
+# Star = "*"
+# +1 to numStates
+# epsilon transition to the previous start state from the new state
+# epsilon transiton from the previous final state to the new state
+# (Note: New state becomes a final state and start state)
 class genfa:
     def __init__(self,s):
         self.newlist = list(s)
 
+    def populateTransitionTable(self):
+        print(self.transition_table)
+    
     def createTransitionTable(self,w,h,symbols):
         transition_table = []
         counter = 0
@@ -14,8 +33,8 @@ class genfa:
             for i in range(len(symbols)+1): #adds a colums in each row. One for each symbols and one for epsilon
                 transition_table[b].insert(counter+1," ")
         self.transition_table = transition_table
-        print(transition_table)   
-               
+        self.populateTransitionTable()
+        
     def checkForMid(self):
         countNumOfParen = 0
         foundFirstClosed = False
@@ -40,13 +59,13 @@ class genfa:
         w, h = NumSymbols, numStates
         self.createTransitionTable(w,h,symbols)
             
-def main():
+def main2():
     s = "((a+b).c)*" 
     newobject = genfa(s)
     newobject.checkForMid()
     return 0
 
-main()
+main2()
 
 
 
