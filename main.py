6f2ecefe71669b5d594f223699ddb6759a8326ea
Katie Pell <57158239+katiepell42@users.noreply.github.com>
2022-05-13 12:19:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,62 +3,97 @@
 # Spring 2022
 
 import outside.inside.eFreeNFA as eFreeNFA # Import Katie's knfa.py file --> uses convert_nfa function
-import outside.inside.genfa as eNFA
 
-# for every character in the input
-# current state on this input gives --> _______
+def get_num(nfa, char):
+  for i in range(len(nfa.symbols)):
+    if nfa.symbols[i] == char:
+      return i
 
-def test2(string, nfa):
-  start = nfa.start_state
-  for char in string:
-    reachable = nfa.transition_table[start][char]
+  return False
 
+def check_membership(nfa, string):
+    current = [nfa.start_state]
 
+    for idx in range(len(string)): # The string without the first character
+      input_char = get_num(nfa, string[idx])
+      # print(input_char)
+      next = []
+      for state in current:
+        # whats the closure of that state?
+        for item in nfa.transition_table[state][input_char]:
+          next.append(item)
+      
+      if len(next) == 0:
+        return False
 
-
-def test(string, nfa):
-    current = [0]*len(nfa.transition_table) # number of states, each has a zero for now
-    starting_state = 0 # this can be initialized to be whatever state it needs to be...
-    current[starting_state] = 1 # mark the starting state as visit-able without even reading input
-    # input_string = "1100" # BUG: THIS REQUIRES THAT INPUTS BE THE ACTUAL CHARACTERS 0 AND 1 THEMSELVES.... HOPEFULLY CAN TRANSLATE LATER...
-
-    while string != "":
-      for i in range(len(current)): # for every state
-        if current[i]: # if it's marked with a 1 in the 'current' vector
-          reachable = nfa.transition_table[i][int(string[0])] # mark all states in the closure of [state][input symbol] as reachable
-          next = [0] * len(nfa.transition_table) # create 'next' vector
-          for i in reachable:
-            next[i] = 1 # mark those as reachable in the 'next' vector
-
-      string = string[1:]
       current = next
 
-    for i in range(len(current)):
-      if current[i] == 1 and i in nfa.final_states:
+    for item in current:
+      if item in nfa.final_states:
         return True
-
+      
     return False
 
 
 def main():
-    # Example from: https://www.geeksforgeeks.org/conversion-of-epsilon-nfa-to-nfa/
+    # Example 1
+    # https://www.geeksforgeeks.org/conversion-of-epsilon-nfa-to-nfa/
     transition_table = [[[], [1], [2]],[[], [0], []],[[3], [4], []],[[2], [], []],[[2], [], []]]
-    # goal = [[[3], [1,4]],[[], [0]],[[3], [4]],[[2], []],[[2], []]]
+    goal = [[[3], [1,4]],[[], [0]],[[3], [4]],[[2], []],[[2], []]]
 
     start_state = 0
     final_states = [2]
     num_states = len(transition_table)
-    symbols = ['a', 'b']
+    symbols = ['0', '1']
 
     M2 = eFreeNFA.NFA(num_states, symbols, transition_table, start_state, final_states).remove_epsilons() # One extra symbol for epsilon
 
-    # print(goal)
-    # print(M2.transition_table)
-    # print(M2.final_states)
 
-    print(test("110", M2))
+    print(M2.symbols)
+    print(M2.transition_table)
+    print(goal)
+
+    test5 = [
+      "10101",
+      "11111",
+      "11101",
+      "10010",
+      "10111",
+      "10110",
+      "01000",
+      "00101",
+      "01011",
+      "00000"
+    ]
+
+    test6 = [
+      "101010",
+      "111111",
+      "100100",
+      "110010",
+      "101001",
+      "001010",
+      "010100",
+      "000110",
+      "001111",
+      "000000"
+    ]
+
+    for string in test5:
+      print(string, check(M2, string))
+    # print(check(M2, "01000"))
+
+    for string in test6:
+      print(string, check(M2, string))
+
 
 
 
 
 main()
+
+
+# 101010 incorrectly gives true (should be false)
+# 10101 incorrectly fives true (should be false)
+
+
